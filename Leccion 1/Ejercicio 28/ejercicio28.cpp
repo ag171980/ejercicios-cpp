851a038ccae3831be3b05d0304a3699fbd4c2880
ag171980 <ag171980@gmail.com>
2022-04-22 02:34:04 -0300

    Subo ejercicio 28

--- Leccion 1/Ejercicio 28/ejercicio28.cpp
+++ Leccion 1/Ejercicio 28/ejercicio28.cpp
@@ -1,45 +1,72 @@
 #include <iostream>
-#include <string.h>
 using namespace std;
-struct Persona {
-	char nombre[20];
-	int fecha_nacimiento;
-};
-int main() {
-	int contador =0,validar=0, mayor_fecha, menor_fecha;
-	char menor_nombre[20];
-	char mayor_nombre[20];
-	Persona p[3];
-	while(contador<3) {
-		cout<<"Ingrese un nombre: ";
-		cin>>p[contador].nombre;
-		cout<<"Ingrese la fecha de nacimiento: ";
-		cin>>p[contador].fecha_nacimiento;
-		contador++;
-	}
-	for(int i =0; i<contador; i++) {
-		if(validar == 0) {
-			strcpy(menor_nombre, p[i].nombre);
-			strcpy(mayor_nombre, p[i].nombre);
-			menor_fecha = p[i].fecha_nacimiento;
-			mayor_fecha = p[i].fecha_nacimiento;
-			validar++;
-		}
-		if(p[i].fecha_nacimiento < menor_fecha) {
-			menor_fecha = p[i].fecha_nacimiento;
-			strcpy(menor_nombre, p[i].nombre);
-		}
-		if(p[i].fecha_nacimiento > mayor_fecha) {
-			mayor_fecha = p[i].fecha_nacimiento;
-			strcpy(mayor_nombre, p[i].nombre);
-		}
-	}
-	cout<<"Persona mas joven: "<<endl;
-	cout<<"Nombre: "<<menor_nombre<<endl;
-	cout<<"Fecha de Nacimiento: "<<menor_fecha<<endl;
-	cout<<"Persona mas vieja: "<<endl;
-	cout<<"Nombre: "<<mayor_nombre<<endl;
-	cout<<"Fecha de Nacimiento: "<<mayor_fecha<<endl;
 
+int main()
+{
+	int conjuntos, valores, contadorConjuntos = 1, contadorValores = 0;
+	int maxValor = 0, sumaValores = 0, cantValoresPositivos = 0;
+	int minValorLote = 0, sumaValoresLote = 0, cantTotalValores = 0, cantValoresNegativosTotal = 0, primerEntrada = 0;
+	cout << "Ingrese cantidad de conjuntos: " << endl;
+	cin >> conjuntos;
+	while (contadorConjuntos <= conjuntos)
+	{
+		cout << "Cantidad de valores para el Conjunto[" << contadorConjuntos << "]" << endl;
+		cin >> valores;
+		int valor;
+		cout << "contValores: " << contadorValores << " valores: " << valores << endl;
+		while (contadorValores < valores)
+		{
+			cout << "Ingrese un valor: " << endl;
+			cin >> valor;
+			if (contadorValores == 0)
+			{
+				if(primerEntrada == 0){
+					minValorLote = valor;
+				}
+				maxValor = valor;
+				primerEntrada++;
+			}
+			if (valor > maxValor)
+			{
+				maxValor = valor;
+			}
+			if (valor < minValorLote)
+			{
+				minValorLote = valor;
+			}
+			if (valor > 0)
+			{
+				cantValoresPositivos++;
+			}
+			if (valor < 0)
+			{
+				cantValoresNegativosTotal++;
+			}
+			cantTotalValores++;
+
+			sumaValores += valor;
+			contadorValores++;
+		}
+		cout << "----------------------------------------" << endl;
+		cout << "Conjunto[" << contadorConjuntos << "]" << endl;
+		cout << "A. Promedio: " << sumaValores / valores << endl;
+		cout << "B. Maximo valor: " << maxValor << endl;
+		cout << "C. Porcentaje de valores positivos: " << (cantValoresPositivos * 100) / valores << "%" << endl;
+		cout << "----------------------------------------" << endl;
+
+		sumaValoresLote += sumaValores;
+
+		sumaValores = 0;
+		maxValor = 0;
+		cantValoresPositivos = 0;
+		contadorValores = 0;
+		contadorConjuntos++;
+	}
+	cout << "Lote de datos: " << endl;
+	cout << "A. Valor promedio: " << sumaValoresLote / cantTotalValores << endl;
+	cout << "B. Porcentaje de valores negativos: " << (cantValoresNegativosTotal * 100) / cantTotalValores << "%" << endl;
+	cout << "C. Valor minimo: " << minValorLote << endl;
+	// cout << "Conjuntos: " << conjuntos << endl;
+	// cout << "Valores: " << valores << endl;
 	return 0;
 }
